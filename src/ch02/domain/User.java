fec0961ec0d245cee4664f7a5eab655c06c6db55
'a' <'a@a.com'>
2023-01-27 16:14:24 +0900

    a

---
+++ src/ch02/domain/User.java
@@ -0,0 +1,26 @@
+package ch02.domain;
+
+import java.time.LocalDate;
+
+public class User {
+	private int userId;
+	private String userName;
+	private LocalDate regDate;
+	
+	public void setUserId(int userId) {
+		this.userId = userId;		
+	}
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	public void setRegDate(LocalDate regDate) {
+		this.regDate = regDate;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%d %s %s", userId, userName, regDate);
+	}
+}
