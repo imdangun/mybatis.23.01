cb720ff567696a2621363dc33c497b5181c6bd64
'a' <'a@a.com'>
2023-01-30 16:50:16 +0900

    a

--- src/ch04/domain/User.java
+++ src/ch04/domain/User.java
@@ -1,11 +1,13 @@
-package ch02.domain;
+package ch04.domain;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class User {
 	private int userId;
 	private String userName;
 	private LocalDate regDate;
+	private List<Post> posts;
 	
 	public User(int userId, String userName, LocalDate regDate) {
 		this.userId = userId;
@@ -15,6 +17,6 @@ public class User {
 	
 	@Override
 	public String toString() {
-		return String.format("%d %s %s", userId, userName, regDate);
+		return String.format("%d %s %s %s", userId, userName, regDate, posts);
 	}
 }
